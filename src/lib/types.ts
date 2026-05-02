197d5884cd465391c47b0c03d8ca0bb1c22d771e
kevinkeyssx <keyss1700@gmail.com>
2026-05-02 02:55:03 -0400

    CloudinaryResource implemented

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,15 +1,48 @@
 export interface ImageFormatOption { value: string; label: string; }
 
 export interface FormState {
-  folder: string;
-  format: string;
-  width: number;
-  height: number;
-  keepDims: boolean;
-  quality: number;
-  transformation: string;
+	folder         : string;
+	format         : string;
+	width          : number;
+	height         : number;
+	keepDims       : boolean;
+	quality        : number;
+	transformation : string;
 }
 
 export type OnChange<T> = (value: T) => void;
 
 export interface DropzoneFileEvent { file: File; }
+
+export interface CloudinaryResource {
+	asset_id      : string;
+	public_id     : string;
+	version       : number;
+	version_id    : string | null;
+	format        : string;
+	resource_type : string;
+	type          : string;
+	created_at    : string;
+	bytes         : number;
+	url           : string;
+	secure_url    : string;
+	folder        : string;
+	etag          : string;
+	status        : string;
+	access_mode   : string;
+	access_control: null;
+	width         : number;
+	height        : number;
+	aspect_ratio  : number;
+	pixels        : number;
+	duration      : null;
+	playback_url  : null;
+	audio         : null;
+	video         : null;
+}
+
+export interface FileManagerResponse {
+	total_count : number;
+	time        : number;
+	resources   : CloudinaryResource[];
+}
