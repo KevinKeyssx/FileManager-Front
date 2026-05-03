161c1888f323985d381125e5544cc0630abb5fb3
kevinkeyssx <keyss1700@gmail.com>
2026-05-03 18:01:10 -0400

    ADMIN_EMAIL implemented

--- src/lib/config/envs/env.ts
+++ src/lib/config/envs/env.ts
@@ -4,34 +4,42 @@ import {
     GOOGLE_CLIENT_ID, 
     GOOGLE_CLIENT_SECRET, 
     BETTER_AUTH_SECRET, 
-    BETTER_AUTH_URL 
+    BETTER_AUTH_URL,
+    ADMIN_EMAIL
 } from "$env/static/private";
 
+
 const envSchema = z.object({
-	BACKEND_URL          : z.string().url(),
+	BACKEND_URL          : z.url(),
     GOOGLE_CLIENT_ID     : z.string().min(1),
     GOOGLE_CLIENT_SECRET : z.string().min(1),
     BETTER_AUTH_SECRET   : z.string().min(1),
-    BETTER_AUTH_URL      : z.string().url()
+    BETTER_AUTH_URL      : z.url(),
+    ADMIN_EMAIL          : z.email()
 });
+
 
 const parsed = envSchema.safeParse({
 	BACKEND_URL,
     GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET,
     BETTER_AUTH_SECRET,
-    BETTER_AUTH_URL
+    BETTER_AUTH_URL,
+    ADMIN_EMAIL
 });
+
 
 if ( !parsed.success ) {
 	console.error( "❌ Invalid environment variables:", parsed.error.format() );
 	throw new Error( "Invalid environment variables" );
 }
 
+
 export const ENVS = {
 	BACKEND_URL          : parsed.data.BACKEND_URL,
     GOOGLE_CLIENT_ID     : parsed.data.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET : parsed.data.GOOGLE_CLIENT_SECRET,
     BETTER_AUTH_SECRET   : parsed.data.BETTER_AUTH_SECRET,
-    BETTER_AUTH_URL      : parsed.data.BETTER_AUTH_URL
+    BETTER_AUTH_URL      : parsed.data.BETTER_AUTH_URL,
+    ADMIN_EMAIL          : parsed.data.ADMIN_EMAIL
 };
