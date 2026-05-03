c9b7f6a64fc14f7866bbb088992f144bfabd4f1a
kevinkeyssx <keyss1700@gmail.com>
2026-05-02 22:07:28 -0400

    Header & Footer implemented

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import { QueryClientProvider } from '@tanstack/svelte-query';
 	import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
-	import favicon from '$lib/assets/favicon.svg';
-	import SEO from '$lib/components/SEO.svelte';
-	import './layout.css';
+
+    import './layout.css';
+
+    import favicon  from '$lib/assets/favicon.svg';
+	import SEO      from '$lib/components/shared/SEO.svelte';
+	import Header   from '$lib/components/home/Header.svelte';
+	import Footer   from '$lib/components/home/Footer.svelte';
+
 
 	let { data, children } = $props();
 </script>
@@ -18,6 +23,18 @@
 </svelte:head>
 
 <QueryClientProvider client={ data.queryClient }>
-	{@render children()}
-	<SvelteQueryDevtools initialIsOpen={ false } />
+	<main class="min-h-screen flex flex-col  text-slate-100 selection:bg-cyan-500/30 selection:text-cyan-200">
+		<!-- Header -->
+		<Header />
+
+		<!-- Main Content -->
+		<section class="flex-1">
+			{@render children( )}
+		</section>
+
+		<!-- Footer -->
+		<Footer />
+	</main>
+
+    <SvelteQueryDevtools initialIsOpen={ false } />
 </QueryClientProvider>
